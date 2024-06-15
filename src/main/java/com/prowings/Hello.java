3dd6dd9509c926d9810080c63342014b277d5e43
JD <jayprowings@gmail.com>
2024-06-15 12:40:32 +0530

    addition method added

--- src/main/java/com/prowings/Hello.java
+++ src/main/java/com/prowings/Hello.java
@@ -18,4 +18,10 @@ public class Hello {
 		System.out.println("Hello!!! Greetings!!!");
 	}
 	
+	public void addition(int n1, int n2)
+	{
+		int res = n1 + n2;
+		System.out.println("Addition is : "+res);
+	}
+	
 }
